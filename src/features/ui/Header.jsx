6c12632e85d5d34e29ee06ff11b52709cfb07438
Diego Carvalho <diego.14crvlho@gmail.com>
2025-03-11 20:48:47 -0400

    wip

--- src/features/ui/Header.jsx
+++ src/features/ui/Header.jsx
@@ -5,10 +5,12 @@ import UserName from "../user/UserName";
 function Header() {
   return (
     //TODO
+
     <header className="flex items-center justify-between border-b border-stone-200 bg-yellow-400 px-4 py-4 uppercase sm:px-6">
       <Link to="/" className="tracking-widest">
         Fast React Pizza Co.
       </Link>
+      //REVIEW
       <SearchOrder />
       <UserName />
     </header>
